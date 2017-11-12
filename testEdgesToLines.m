4837e5f15968c40b45bd88abe0c1a7ca99eee02a
jimiwesterholm <jimi.westerholm@gmail.com>
2017-11-12 17:11:19 +0000

    change way of mesuring in edgesToLines

--- testEdgesToLines.m
+++ testEdgesToLines.m
@@ -1,11 +1,11 @@
-function [ ratios, failures ] = testEdgesToLines( natural, manmade, offset, amount)
+function [ ratios, failures ] = testEdgesToLines( natural, manmade, offset, amount, measure)
 %TESTEDGESTOLINES Summary of this function goes here
 %   Detailed explanation goes here
 ratios = zeros(amount, 2);
 failures = zeros(amount, 1);
 for i=(offset+1) : amount + offset
-    a = edgesToLinesRatio(rgb2gray(manmade{1, i}));
-    b = edgesToLinesRatio(rgb2gray(natural{1, i}));
+    a = edgesToLinesRatio(rgb2gray(manmade{1, i}), measure);
+    b = edgesToLinesRatio(rgb2gray(natural{1, i}), measure);
     if a > b
         failures(i) = 1;
     end

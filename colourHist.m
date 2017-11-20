6742f2cc93c44d5103fb6b8abc0b163d3ccf6a1c
xyzniu <xinyue.z.niu@gmail.com>
2017-11-20 18:51:24 +0000

    just a misunderstanding-.-

--- colourHist.m
+++ colourHist.m
@@ -7,8 +7,7 @@ function h = colourHist(img, binSize)
 
 [x, y, ~] = size(img);
 h = zeros([binSize*binSize*binSize, 1]);
-div = 256/binSize;
-% div = 256/(binSize - 1);
+div = 256/(binSize - 1);
 
 for i=1 : x
     for j=1 : y

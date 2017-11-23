3fd9cd0f3385a25cb4ebbb5d63320a1962882a45
jimiwesterholm <jimi.westerholm@gmail.com>
2017-11-23 13:31:22 +0000

    longest line fix
    better now, divides by mean of dimensions

--- longestLine.m
+++ longestLine.m
@@ -6,19 +6,19 @@ edges = edge(image, 'canny');
 [ihough, theta, rho]= hough(edges);
 
 %peaks = houghpeaks(ihough, 1073741824);
-peaks = houghpeaks(ihough, 10000, 'Threshold', 0.5*max(ihough(:)));
+peaks = houghpeaks(ihough, 10, 'Threshold', 0.5*max(ihough(:)));
 
 lines = houghlines(edges, theta, rho, peaks, 'MinLength', 5, 'FillGap',5);
 
 %totalEdges = sum(sum(edges));
-%imgPixels = size(image(:));
+imgPixels = size(image);
 
 dist = zeros(length(lines), 1);
 for i=1 : length(lines)
     dist(i) = sqrt((lines(i).point1(1) - lines(i).point2(1))^2 + (lines(i).point1(2) - lines(i).point2(2))^2);
 end
 dist = sort(dist, 'descend');
-result = dist(1);
+result = dist(1) / mean(imgPixels);
 
 %ratio =  dist(1) + dist(2) + dist(3);
 %ratio = imgPixels(1) / max(dist(:));
